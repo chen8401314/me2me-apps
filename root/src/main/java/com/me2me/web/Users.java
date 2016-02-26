d4929e170dc910fee0eef906606e456b6435c639
peter <25291360@qq.com>
2016-02-26 21:49:28 +0800

    commit interface api demos

--- root/src/main/java/com/me2me/web/Users.java
+++ root/src/main/java/com/me2me/web/Users.java
@@ -1,7 +1,16 @@
 package com.me2me.web;
 
+import com.me2me.common.web.Response;
+import com.me2me.user.dto.UserLoginDto;
+import com.me2me.user.dto.UserSignUpDto;
+import com.me2me.user.service.UserService;
+import com.me2me.web.request.LoginRequest;
+import com.me2me.web.request.SignUpRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -13,10 +22,32 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/api/user")
 public class Users {
 
-    @RequestMapping(value = "/say")
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 用户注册接口
+     * @return
+     */
+    @RequestMapping(value = "/signUp",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public String say(){
-        return "Hello";
+    public Response signUp(SignUpRequest request){
+        UserSignUpDto userSignUpDto = new UserSignUpDto();
+        // todo add request params
+        return userService.signUp(userSignUpDto);
+    }
+
+    /**
+     * 用户登录
+     * @return
+     */
+    @RequestMapping(value = "/login",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Response login(LoginRequest request){
+        UserLoginDto userLoginDto = new UserLoginDto();
+        userLoginDto.setUserName(request.getUserName());
+        userLoginDto.setEncrypt(request.getEncrypt());
+        return userService.login(userLoginDto);
     }
 
 }
