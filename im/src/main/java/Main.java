2f10e5737cf2eb637cebd3a1385fca364031f6e6
peter <25291360@qq.com>
2016-03-07 11:35:07 +0800

    im model created

--- im/src/main/java/Main.java
+++ im/src/main/java/Main.java
@@ -6,6 +6,7 @@ import org.jivesoftware.smack.chat.ChatManager;
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+import org.jivesoftware.smackx.muc.packet.GroupChatInvitation;
 
 import java.io.IOException;
 
@@ -26,14 +27,18 @@ public class Main {
         config.setDebuggerEnabled(false);
         XMPPTCPConnection conn = new XMPPTCPConnection(config.build());
         conn.connect().login();
-        Chat chat = ChatManager.getInstanceFor(conn).createChat("abc0@iz23a2nwds6z");
+        Chat chat = ChatManager.getInstanceFor(conn).createChat("abc3@iz23a2nwds6z");
         // chat.sendMessage("Hello");
         Message message = new Message();
         message.setSubject("tips");
         message.setFrom("abc1000");
         message.setBody("hello");
-        message.setType(Message.Type.chat);
+        message.setType(Message.Type.headline);
         chat.sendMessage(message);
+
+
+
+
 
     }
 }
