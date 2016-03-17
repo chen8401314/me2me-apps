37b3a3ffeacbfd37ef9003272d8bea7454ef841c
peter <25291360@qq.com>
2016-03-17 17:54:16 +0800

    add logs

--- common/src/main/java/com/me2me/common/web/ResponseStatus.java
+++ common/src/main/java/com/me2me/common/web/ResponseStatus.java
@@ -25,6 +25,12 @@ public enum  ResponseStatus {
 
     USER_VERIFY_CHECK_SUCCESS("用户爱好修改成功","user verify check success",2005),
 
+    USER_MODIFY_AVATAR_SUCCESS("用户头像修改成功","user modify avatar success",2006),
+
+    USER_MODIFY_NICK_NAME_SUCCESS("用昵称像修改成功","user modify nickName success",2007),
+
+    USER_MODIFY_PROFILE_SUCCESS("用户资料修改成功","user modify profile success",2008),
+
     USER_ADD_FRIEND_SUCCESS("用户创建好友成功","user create friend success",20020),
 
     USER_REMOVE_FRIEND_SUCCESS("用户删除好友成功","user remove friend success",20021),
@@ -34,12 +40,6 @@ public enum  ResponseStatus {
     ADD_GROUP_MEMBER_SUCCESS("添加群成员成功","add group member success",20023),
 
     REMOVE_GROUP_MEMBER_SUCCESS("移除群成员成功","remove group member success",20023),
-
-    USER_MODIFY_AVATAR_SUCCESS("用户头像修改成功","user modify avatar success",2006),
-
-    USER_MODIFY_NICK_NAME_SUCCESS("用昵称像修改成功","user modify nickName success",2007),
-
-    USER_MODIFY_PROFILE_SUCCESS("用户资料修改成功","user modify profile success",2008),
 
     USER_PASSWORD_ERROR("用户密码错误","user password error",5000),
 
@@ -51,12 +51,11 @@ public enum  ResponseStatus {
 
     USER_VERIFY_ERROR("验证码接口调用错误","user verify error",5004),
 
-    USER_ADD_FRIEND_ERROR("不能自己添加自己为好友","can't add yourself",50020),
-
     USER_MODIFY_ENCRYPT_PASSWORD_NOT_SAME_ERROR("两次密码输入不一致"," user the two passwords are not the same",5005),
 
-    USER_MODIFY_USER_PROFILE_ERROR("用户信息修改错误","user modify user profile error",5006);
+    USER_MODIFY_USER_PROFILE_ERROR("用户信息修改错误","user modify user profile error",5006),
 
+    USER_ADD_FRIEND_ERROR("不能自己添加自己为好友","can't add yourself",50020);
 
     public String message;
 
