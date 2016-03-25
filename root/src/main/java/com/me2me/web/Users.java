cb76577fe3733feea611f70f4e43307554e00d13
daibaolei <63914178@qq.com>
2016-03-25 16:47:52 +0800

    gender

--- root/src/main/java/com/me2me/web/Users.java
+++ root/src/main/java/com/me2me/web/Users.java
@@ -114,7 +114,7 @@ public class Users {
         modifyUserProfileDto.setAction(request.getAction());
         modifyUserProfileDto.setUserName(request.getUserName());
         modifyUserProfileDto.setBearStatus(request.getBearStatus());
-        modifyUserProfileDto.setGender(request.getGander());
+        modifyUserProfileDto.setGender(request.getGender());
         modifyUserProfileDto.setIndustry(request.getIndustry());
         modifyUserProfileDto.setMarriageStatus(request.getMarriageStatus());
         modifyUserProfileDto.setStartId(request.getStartId());
