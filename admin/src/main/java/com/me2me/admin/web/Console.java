0de44a5ad3d003339645ebcd044f411ac44cd389
peter <25291360@qq.com>
2016-03-30 17:44:26 +0800

    commit ui and css and scripts

--- admin/src/main/java/com/me2me/admin/web/Console.java
+++ admin/src/main/java/com/me2me/admin/web/Console.java
@@ -1,9 +1,18 @@
 package com.me2me.admin.web;
 
+import com.me2me.content.service.ContentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 上海拙心网络科技有限公司出品
@@ -14,10 +23,42 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/console")
 public class Console  {
 
-    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    @Autowired
+    private ContentService contentService;
+
+    @RequestMapping(value = "/signIn")
+    public String signIn(){
+        return "login";
+    }
+
+    @RequestMapping(value = "/index")
     public String index(){
-        System.out.println("Hello");
         return "index";
+    }
+
+    @RequestMapping(value = "/data")
+    public ModelAndView data(){
+        ModelAndView mv = new ModelAndView("data");
+        List<String> list = new ArrayList<String>();
+        for(int i = 0;i<100; i++){
+            list.add(i+"");
+        }
+        mv.addObject("data",list);
+        return mv;
+    }
+
+    @RequestMapping(value = "/publish")
+    public String publish(){
+        return "publish";
+    }
+
+    @RequestMapping(value = "/forward")
+    public ModelAndView forward(){
+        ModelAndView mv = new ModelAndView("forward");
+//        Map map = new HashMap<>();
+//        map.put("coverImage","");
+        // mv.addObject("data",map);
+        return mv;
     }
 
 
