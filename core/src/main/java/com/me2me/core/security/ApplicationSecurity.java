b218bef10ac21fb9670fb0be485b9feb3243d995
peter <25291360@qq.com>
2016-04-05 19:39:36 +0800

    add clean user tips

--- core/src/main/java/com/me2me/core/security/ApplicationSecurity.java
+++ core/src/main/java/com/me2me/core/security/ApplicationSecurity.java
@@ -40,20 +40,18 @@ public class ApplicationSecurity {
     }
 
     static void testSign() throws Exception {
-        System.err.println("私钥加密——公钥解密");
         String source = "Hello";
-        System.out.println("原文字：\r\n" + source);
         byte[] data = source.getBytes();
         byte[] encodedData = RSAUtils.encryptByPrivateKey(data, privateKey);
-        System.out.println("加密后：\r\n" + new String(encodedData));
+        // System.out.println("加密后：\r\n" + new String(encodedData));
         byte[] decodedData = RSAUtils.decryptByPublicKey(encodedData, publicKey);
         String target = new String(decodedData);
-        System.out.println("解密后: \r\n" + target);
-        System.err.println("私钥签名——公钥验证签名");
+        // System.out.println("解密后: \r\n" + target);
+        // System.err.println("私钥签名——公钥验证签名");
         String sign = RSAUtils.sign(encodedData, privateKey);
         System.err.println("签名:\r" + sign);
         boolean status = RSAUtils.verify(encodedData, publicKey, sign);
-        System.err.println("验证结果:\r" + status);
+        // System.err.println("验证结果:\r" + status);
     }
 
 }
