7565e12dae484afb850223da60ddaf37c29165a3
daibaolei <63914178@qq.com>
2016-04-06 11:51:13 +0800

    发布成功修改

--- content/src/main/java/com/me2me/content/service/ContentServiceImpl.java
+++ content/src/main/java/com/me2me/content/service/ContentServiceImpl.java
@@ -141,7 +141,7 @@ public class ContentServiceImpl implements ContentService {
         ContentUserLikesCount contentUserLikesCount = new ContentUserLikesCount();
         contentUserLikesCount.setCid(content.getId());
         contentUserLikesCount.setTid(contentTags.getId());
-        contentMybatisDao.addContentUserLikesCount(contentUserLikesCount);
+       // contentMybatisDao.addContentUserLikesCount(contentUserLikesCount);
 
         return Response.success(ResponseStatus.PUBLISH_ARTICLE_SUCCESS.status,ResponseStatus.PUBLISH_ARTICLE_SUCCESS.message,contentDto);
 }
