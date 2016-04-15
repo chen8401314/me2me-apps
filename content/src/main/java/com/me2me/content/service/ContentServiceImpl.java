38a8fe23d7ef0ee47b3c1786f3245f92aa37b25e
peter <25291360@qq.com>
2016-04-15 18:24:52 +0800

    add user follows methods

--- content/src/main/java/com/me2me/content/service/ContentServiceImpl.java
+++ content/src/main/java/com/me2me/content/service/ContentServiceImpl.java
@@ -463,15 +463,14 @@ public class ContentServiceImpl implements ContentService {
         List<ContentImage> list = contentMybatisDao.getContentImages(id);
         for (ContentImage contentImage : list){
             if(contentImage.getCover() == 1){
-                if(content.getType()==Specification.ArticleType.EDITOR.index){
-                    contentH5Dto.setCoverImage(content.getConverImage());
-                }else {
-                    contentH5Dto.setCoverImage(Constant.QINIU_DOMAIN + "/" + contentImage.getImage());
-                }
+                contentH5Dto.setCoverImage(Constant.QINIU_DOMAIN + "/" + contentImage.getImage());
             }
             if(contentImage.getCover() == 0){
                 contentH5Dto.getImageUrls().add(Constant.QINIU_DOMAIN + "/" + contentImage.getImage());
             }
+        }
+        if(content.getType()==Specification.ArticleType.EDITOR.index){
+            contentH5Dto.setCoverImage(content.getConverImage());
         }
         contentH5Dto.setTitle(content.getTitle());
         contentH5Dto.setType(content.getType());
