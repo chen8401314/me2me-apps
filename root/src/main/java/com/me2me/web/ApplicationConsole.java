e534cd6a8bdc4e18af704fbdf7bb750c73768411
peter <25291360@qq.com>
2016-04-25 11:24:13 +0800

    add user follows methods

---
+++ root/src/main/java/com/me2me/web/ApplicationConsole.java
@@ -0,0 +1,43 @@
+package com.me2me.web;
+
+import com.me2me.common.web.Response;
+import com.me2me.common.web.Specification;
+import com.me2me.user.dto.*;
+import com.me2me.user.service.UserService;
+import com.me2me.web.request.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 上海拙心网络科技有限公司出品
+ * Author: 赵朋扬
+ * Date: 2016/2/25.
+ */
+@Controller
+@RequestMapping(value = "/api/console")
+public class ApplicationConsole extends BaseController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 用户注册接口
+     * @return
+     */
+    @RequestMapping(value = "/bindAccount",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Response bindAccount(BindAccountRequest bindAccountRequest){
+        UserSignUpDto userSignUpDto = new UserSignUpDto();
+        userSignUpDto.setMobile("18000000000");
+        userSignUpDto.setGender(0);
+        userSignUpDto.setEncrypt("123456");
+        userSignUpDto.setNickName(bindAccountRequest.getNickName());
+        return userService.signUp(userSignUpDto);
+    }
+
+
+}
