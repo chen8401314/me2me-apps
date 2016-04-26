1b14533e201307c9320e46fb0752607d563a7f14
peter <25291360@qq.com>
2016-04-26 20:50:15 +0800

    add user follows methods

---
+++ activity-api/src/main/java/com/me2me/activity/dto/CreateActivityDto.java
@@ -0,0 +1,28 @@
+package com.me2me.activity.dto;
+
+import com.me2me.common.web.BaseEntity;
+
+import java.util.Date;
+
+/**
+ * 上海拙心网络科技有限公司出品
+ * Author: 赵朋扬
+ * Date: 2016/4/26.
+ */
+public class CreateActivityDto implements BaseEntity {
+
+    private String activityTitle;
+
+    private String activityContent;
+
+    private String ext1;
+
+    private String ext2;
+
+    private Date startTime;
+
+    private Date updateTime;
+
+
+
+}
---
+++ activity-api/src/main/java/com/me2me/activity/service/ActivityService.java
@@ -0,0 +1,20 @@
+package com.me2me.activity.service;
+
+import com.me2me.common.web.Response;
+
+/**
+ * 上海拙心网络科技有限公司出品
+ * Author: 赵朋扬
+ * Date: 2016/4/11.
+ */
+public interface ActivityService {
+
+
+    /**
+     * 创建活动
+     * @return
+     */
+    Response createActivity();
+
+
+}
