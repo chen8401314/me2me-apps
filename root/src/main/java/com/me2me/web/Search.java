f1eab06f0ea1e9b9aaeaf9097c8dabf66d5e9a21
peter <25291360@qq.com>
2016-04-28 16:47:18 +0800

    add search model

---
+++ root/src/main/java/com/me2me/web/Search.java
@@ -0,0 +1,26 @@
+package com.me2me.web;
+
+import com.me2me.common.web.Response;
+import com.me2me.live.dto.CreateLiveDto;
+import com.me2me.live.dto.GetLiveTimeLineDto;
+import com.me2me.live.dto.SpeakDto;
+import com.me2me.live.service.LiveService;
+import com.me2me.web.request.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 上海拙心网络科技有限公司出品
+ * Author: 代宝磊
+ * Date: 2016/4/11
+ * Time :18:09
+ */
+@Controller
+@RequestMapping(value = "/api/search")
+public class Search extends BaseController {
+
+}
---
+++ search/src/main/java/com/me2me/search/dao/ActivityMybatisDao.java
@@ -0,0 +1,54 @@
+package com.me2me.search.dao;
+
+import com.google.common.base.Strings;
+import com.me2me.activity.mapper.ActivityMapper;
+import com.me2me.activity.model.Activity;
+import com.me2me.activity.model.ActivityExample;
+import com.me2me.activity.model.ActivityWithBLOBs;
+import com.me2me.common.web.Specification;
+import com.me2me.user.dto.*;
+import com.me2me.user.mapper.*;
+import com.me2me.user.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * 上海拙心网络科技有限公司出品
+ * Author: 赵朋扬
+ * Date: 2016/2/26.
+ */
+@Repository
+public class ActivityMybatisDao {
+
+    @Autowired
+    private ActivityMapper activityMapper;
+
+
+    public void saveActivity(ActivityWithBLOBs activity){
+        activityMapper.insertSelective(activity);
+    }
+
+
+    public List<ActivityWithBLOBs> showActivity(int page, int pageSize,String keyword) {
+        ActivityExample example = new ActivityExample();
+        ActivityExample.Criteria criteria = example.createCriteria();
+        criteria.andStatusEqualTo(0);
+        if(!Strings.isNullOrEmpty(keyword)){
+            criteria.andActivityTitleLike("%"+keyword+"%");
+        }
+        example.setOrderByClause("issue desc limit "+ ((page-1)*pageSize) + " , "+pageSize );
+        return activityMapper.selectByExampleWithBLOBs(example);
+    }
+
+    public int total(String keyword) {
+        ActivityExample example = new ActivityExample();
+        ActivityExample.Criteria criteria = example.createCriteria();
+        criteria.andStatusEqualTo(0);
+        if(!Strings.isNullOrEmpty(keyword)){
+            criteria.andActivityTitleLike("%"+keyword+"%");
+        }
+        return activityMapper.countByExample(example);
+    }
+}
