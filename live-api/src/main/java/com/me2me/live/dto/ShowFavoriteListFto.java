4e35db3ed5021809531d90953ee8fb1857daeecd
daibaolei <63914178@qq.com>
2016-05-14 14:32:48 +0800

    2.0.2

--- live-api/src/main/java/com/me2me/live/dto/ShowFavoriteListFto.java
+++ live-api/src/main/java/com/me2me/live/dto/ShowFavoriteListFto.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Data
 public class ShowFavoriteListFto implements BaseEntity {
 
-    private  List<FavoriteUser> liveElements = Lists.newArrayList();
+    private  List<FavoriteUser> favoriteUserElements = Lists.newArrayList();
 
     public static FavoriteUser createElement(){
         return new FavoriteUser();
