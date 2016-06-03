e98a94e3a5b1729f1a4113513db7bbe85921ad6b
daibaolei <63914178@qq.com>
2016-06-03 16:02:51 +0800

    2.0.3  不传deviceNo时候系统正常

--- user/src/main/java/com/me2me/user/service/UserServiceImpl.java
+++ user/src/main/java/com/me2me/user/service/UserServiceImpl.java
@@ -811,7 +811,6 @@ public class UserServiceImpl implements UserService {
                 pushMessageAndroidDto.setTitle(pushMessageDto.getContent());
                 pushMessageAndroidDto.setToken(device.getDeviceNo());
                 pushMessageAndroidDto.setMessageType(type);
-                pushMessageAndroidDto.setTitle("米汤友情提示");
                 pushMessageAndroidDto.setContent(pushMessageDto.getContent());
                 PushLogDto pushLogDto = xgPushService.pushSingleDevice(pushMessageAndroidDto);
                 if (pushLogDto != null) {
@@ -822,7 +821,6 @@ public class UserServiceImpl implements UserService {
                 PushMessageIosDto pushMessageIosDto = new PushMessageIosDto();
                 pushMessageIosDto.setTitle(pushMessageDto.getContent());
                 pushMessageIosDto.setToken(device.getDeviceNo());
-                pushMessageIosDto.setTitle("米汤友情提示");
                 pushMessageIosDto.setContent(pushMessageDto.getContent());
                 PushLogDto pushLogDto = xgPushService.pushSingleDeviceIOS(pushMessageIosDto);
                 if (pushLogDto != null) {
