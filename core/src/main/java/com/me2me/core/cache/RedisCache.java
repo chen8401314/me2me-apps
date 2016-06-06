a892eb5d2052ae983431dde0b20fe9016397d6a9
peter <25291360@qq.com>
2016-06-06 14:03:27 +0800

    modify pom for service

--- core/src/main/java/com/me2me/core/cache/RedisCache.java
+++ core/src/main/java/com/me2me/core/cache/RedisCache.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
 
 import java.util.Set;
 
