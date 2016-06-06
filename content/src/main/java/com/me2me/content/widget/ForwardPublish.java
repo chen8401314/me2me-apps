baa45ed6ad3f95994e786ed470f50a77ceddfbff
daibaolei <63914178@qq.com>
2016-06-06 22:54:25 +0800

    2.0.3

--- content/src/main/java/com/me2me/content/widget/ForwardPublish.java
+++ content/src/main/java/com/me2me/content/widget/ForwardPublish.java
@@ -2,12 +2,14 @@ package com.me2me.content.widget;
 
 import com.me2me.common.web.Response;
 import com.me2me.content.dto.ContentDto;
+import org.springframework.stereotype.Component;
 
 /**
  * 上海拙心网络科技有限公司出品
  * Author: 赵朋扬
  * Date: 2016/6/6.
  */
+@Component
 public class ForwardPublish extends AbstractPublish implements Publish {
 
     public Response publish(ContentDto contentDto){
