e921ea9076ce995c6b560051ab104f82f9a1d411
daibaolei <63914178@qq.com>
2016-06-07 19:51:35 +0800

    2.0.3

--- root/src/main/java/com/me2me/web/Contents.java
+++ root/src/main/java/com/me2me/web/Contents.java
@@ -218,7 +218,7 @@ public class Contents extends BaseController {
         reviewDto.setUid(request.getUid());
         reviewDto.setCid(request.getCid());
         reviewDto.setReview(request.getReview());
-        reviewDto.setType(request.getType());
+        reviewDto.setType(1);
         return contentService.createReview(reviewDto);
     }
 
