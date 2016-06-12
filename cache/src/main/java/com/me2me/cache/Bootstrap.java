98c999295497c7d2d15597174400d2c1e7a4d1e2
peter <25291360@qq.com>
2016-06-12 20:54:09 +0800

    uninstall search-api

---
+++ cache/src/main/java/com/me2me/cache/Bootstrap.java
@@ -0,0 +1,27 @@
+package com.me2me.cache;
+
+import com.google.common.eventbus.AsyncEventBus;
+import com.google.common.eventbus.EventBus;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * 上海拙心网络科技有限公司出品
+ * Author: 赵朋扬
+ * Date: 2016/2/26.
+ */
+public class Bootstrap {
+    public static void main(String[] args) throws InterruptedException {
+//        final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring/user-dubbo-provider.xml","classpath:spring/user-dubbo-consumer.xml");
+//        ctx.start();
+//        Runtime.getRuntime().addShutdownHook(new Thread(){
+//            @Override
+//            public void run() {
+//                ctx.close();
+//            }
+//        });
+//        CountDownLatch countDownLatch = new CountDownLatch(1);
+//        countDownLatch.await();
+    }
+}
---
+++ cache/src/main/java/com/me2me/cache/service/CacheServiceImpl.java
@@ -0,0 +1,12 @@
+package com.me2me.cache.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 上海拙心网络科技有限公司出品
+ * Author: 赵朋扬
+ * Date: 2016/6/12.
+ */
+@Service
+public class CacheServiceImpl implements CacheService {
+}
