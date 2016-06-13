1916cdd9c3be5a55c75d996a662b9d6b90b94c43
peter <25291360@qq.com>
2016-06-13 19:07:05 +0800

    add logback implements by slf4j

--- cache-api/src/main/java/com/me2me/cache/service/CacheService.java
+++ cache-api/src/main/java/com/me2me/cache/service/CacheService.java
@@ -1,5 +1,7 @@
 package com.me2me.cache.service;
 
+import java.util.Set;
+
 /**
  * 上海拙心网络科技有限公司出品
  * Author: 赵朋扬
@@ -8,6 +10,55 @@ package com.me2me.cache.service;
 public interface CacheService {
 
 
+    /**
+     * 设置普通键值对
+     * @param key
+     * @param value
+     */
     void set(String key,String value);
+
+    /**
+     * set集合操作
+     * @param key
+     * @param values
+     */
+    void sadd(String key,String ... values);
+
+    /**
+     * 获取set集合成员
+     * @param key
+     * @return
+     */
+    Set<String> smembers(String key);
+
+    /**
+     * 清空缓存数据（当心使用）
+     */
+    void flushDB();
+
+    /**
+     * list操作
+     * @param key
+     * @param value
+     */
+    void lPush(String key,String ... value);
+    /**
+     * list操作
+     * @param key
+     * @param value
+     */
+    void rPush(String key,String ... value);
+    /**
+     * list操作
+     * @param key
+     */
+    String lPop(final String key);
+
+    /**
+     * 指定某个键的过期时间
+     * @param key
+     * @param timeout
+     */
+    void expire(String key,int timeout);
 
 }
