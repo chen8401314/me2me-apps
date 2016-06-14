98e4aeb971d5eff488161c8979f2545a91c81704
daibaolei <63914178@qq.com>
2016-06-14 19:26:15 +0800

    2.0.4

--- root/src/main/java/com/me2me/web/Live.java
+++ root/src/main/java/com/me2me/web/Live.java
@@ -66,7 +66,7 @@ public class Live extends BaseController {
      */
     @RequestMapping(value = "/barrage",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public Response liveTimelineBarrage(BarrageRequest request){
+    public Response barrage(BarrageRequest request){
         BarrageDto barrageDto = new BarrageDto();
         barrageDto.setSinceId(request.getSinceId());
         barrageDto.setTopicId(request.getTopicId());
