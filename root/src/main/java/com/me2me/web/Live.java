25929cbc803489707ca844724e6aed7c093176fd
daibaolei <63914178@qq.com>
2016-06-14 19:49:02 +0800

    2.0.4

--- root/src/main/java/com/me2me/web/Live.java
+++ root/src/main/java/com/me2me/web/Live.java
@@ -13,9 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.util.Calendar;
-import java.util.Date;
 
 /**
  * 上海拙心网络科技有限公司出品
