18f602e802b9fbd4cb5e4fba1e594c01b6f76347
daibaolei <63914178@qq.com>
2016-06-23 15:28:20 +0800

    2.0.4 live  speak

--- live/src/main/java/com/me2me/live/service/LiveServiceImpl.java
+++ live/src/main/java/com/me2me/live/service/LiveServiceImpl.java
@@ -214,7 +214,7 @@ public class LiveServiceImpl implements LiveService {
     @Override
     public Response speak(SpeakDto speakDto) {
         log.info("speak start ...");
-        if(speakDto.getType() != Specification.LiveSpeakType.LIKES.index &&speakDto.getType() != Specification.LiveSpeakType.SUBSCRIBED.index ) {
+        if(speakDto.getType() != Specification.LiveSpeakType.LIKES.index &&speakDto.getType() != Specification.LiveSpeakType.SUBSCRIBED.index && speakDto.getType() != Specification.LiveSpeakType.SHARE.index  && speakDto.getType() != Specification.LiveSpeakType.FOLLOW.index) {
             TopicFragment topicFragment = new TopicFragment();
             topicFragment.setFragmentImage(speakDto.getFragmentImage());
             topicFragment.setFragment(speakDto.getFragment());
