922e40392b4cbe03c993b2ffd77b473ae80ef9ae
peter <25291360@qq.com>
2016-06-27 11:34:05 +0800

    modify report

---
+++ sns-api/src/main/java/com/me2me/sns/service/SnsService.java
@@ -0,0 +1,9 @@
+package com.me2me.sns.service;
+
+/**
+ * 上海拙心网络科技有限公司出品
+ * Author: 赵朋扬
+ * Date: 2016/6/27.
+ */
+public interface SnsService {
+}
---
+++ sns/src/main/java/com/me2me/sns/service/SnsServiceImpl.java
@@ -0,0 +1,15 @@
+package com.me2me.sns.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 上海拙心网络科技有限公司出品
+ * Author: 赵朋扬
+ * Date: 2016/6/27.
+ */
+@Service
+public class SnsServiceImpl implements SnsService {
+
+
+
+}
