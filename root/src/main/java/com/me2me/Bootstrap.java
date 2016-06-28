276134cb347ced2813c2baebf6623432d911e80c
peter <25291360@qq.com>
2016-06-28 23:48:04 +0800

    modify report

--- root/src/main/java/com/me2me/Bootstrap.java
+++ root/src/main/java/com/me2me/Bootstrap.java
@@ -1,21 +1,21 @@
-package com.me2me;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
-
-/**
- * 上海拙心网络科技有限公司出品
- * Author: 赵朋扬
- * Date: 2016/6/26.
- */
-@SpringBootApplication
-@ImportResource(locations = {"classpath:spring/root-context.xml"})
-@ComponentScan("com.me2me")
-public class Bootstrap {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Bootstrap.class,args);
-    }
-}
+//package com.me2me;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.ImportResource;
+//
+///**
+// * 上海拙心网络科技有限公司出品
+// * Author: 赵朋扬
+// * Date: 2016/6/26.
+// */
+//@SpringBootApplication
+//@ImportResource(locations = {"classpath:spring/root-context.xml"})
+//@ComponentScan("com.me2me")
+//public class Bootstrap {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Bootstrap.class,args);
+//    }
+//}
