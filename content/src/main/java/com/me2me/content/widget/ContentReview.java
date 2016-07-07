25a7e94d64f36e9240c17f74567ab2613f739418
daibaolei <63914178@qq.com>
2016-07-07 17:11:22 +0800

    2.0.5

--- content/src/main/java/com/me2me/content/widget/ContentReview.java
+++ content/src/main/java/com/me2me/content/widget/ContentReview.java
@@ -42,7 +42,7 @@ public class ContentReview implements Review{
         if(reviewDto.getIsAt() == 1) {
             //兼容老版本
             if(reviewDto.getAtUid() != 0) {
-                if(reviewDto.getAtUid() != reviewDto.getUid()) {
+                if(reviewDto.getAtUid() != content.getUid()) {
                     contentService.remind(content, reviewDto.getUid(), Specification.UserNoticeType.REVIEW.index, reviewDto.getReview(), reviewDto.getAtUid());
                     userService.push(reviewDto.getAtUid(), reviewDto.getUid(), Specification.PushMessageType.AT.index, reviewDto.getReview());
                 }
