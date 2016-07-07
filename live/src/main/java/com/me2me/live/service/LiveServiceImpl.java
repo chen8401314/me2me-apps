882e4023c955f3f2f741a240ef94dfc81eb45e39
daibaolei <63914178@qq.com>
2016-07-07 14:57:43 +0800

    2.0.5

--- live/src/main/java/com/me2me/live/service/LiveServiceImpl.java
+++ live/src/main/java/com/me2me/live/service/LiveServiceImpl.java
@@ -529,7 +529,7 @@ public class LiveServiceImpl implements LiveService {
             if (liveFavorite != null) {
                 liveMybatisDao.deleteLiveFavorite(liveFavorite);
             }
-            if(liveMybatisDao.getFavoriteDelete(uid, topicId) != null) {
+            if(liveMybatisDao.getFavoriteDelete(uid, topicId) == null) {
                 liveMybatisDao.createFavoriteDelete(uid, topicId);
                 if ((content.getFavoriteCount() - 1) < 0) {
                     content.setFavoriteCount(0);
@@ -711,9 +711,9 @@ public class LiveServiceImpl implements LiveService {
         return liveMybatisDao.getMyTopic(uid);
     }
 
-
-    public void deleteLiveFavoriteByUid(long uid,long topicId){
-       liveMybatisDao.deleteLiveFavoriteByUid(uid,topicId);
+    @Override
+    public List<Topic> getMyTopic4Follow(long uid) {
+        return liveMybatisDao.getMyTopic4Follow(uid);
     }
 
 }
