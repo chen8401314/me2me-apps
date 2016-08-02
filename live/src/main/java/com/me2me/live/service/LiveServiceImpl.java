071a16649e6c42964338dc71f2cef1cacb7df8c0
peter <25291360@qq.com>
2016-08-02 18:54:37 +0800

    fix redis operation exception

--- live/src/main/java/com/me2me/live/service/LiveServiceImpl.java
+++ live/src/main/java/com/me2me/live/service/LiveServiceImpl.java
@@ -247,7 +247,7 @@ public class LiveServiceImpl implements LiveService {
             List<LiveFavorite> liveFavorites = liveMybatisDao.getFavoriteList(speakDto.getTopicId());
             for(LiveFavorite liveFavorite : liveFavorites) {
                 MySubscribeCacheModel cacheModel = new MySubscribeCacheModel(liveFavorite.getUid(), liveFavorite.getTopicId() + "", "1");
-                log.info("speak:0 start update hset cache key{} field {} value {}",cacheModel.getKey(),cacheModel.getField(),cacheModel.getValue());
+                log.info("speak by master start update hset cache key{} field {} value {}",cacheModel.getKey(),cacheModel.getField(),cacheModel.getValue());
                 cacheService.hSet(cacheModel.getKey(), cacheModel.getField(), cacheModel.getValue());
             }
         }else{
@@ -255,7 +255,7 @@ public class LiveServiceImpl implements LiveService {
             List<UserFollow> list = userService.getFans(topic.getUid());
             for(UserFollow userFollow : list) {
                 MySubscribeCacheModel cacheModel = new MySubscribeCacheModel(userFollow.getSourceUid(), topic.getId() + "", "1");
-                log.info("speak:1 start update hset cache key{} field {} value {}",cacheModel.getKey(),cacheModel.getField(),cacheModel.getValue());
+                log.info("speak by other start update hset cache key{} field {} value {}",cacheModel.getKey(),cacheModel.getField(),cacheModel.getValue());
                 cacheService.hSet(cacheModel.getKey(), cacheModel.getField(), cacheModel.getValue());
             }
         }
