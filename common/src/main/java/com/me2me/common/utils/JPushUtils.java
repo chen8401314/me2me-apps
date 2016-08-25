22e3f48ba9c407d8a0d60e4ff1a93166248e663f
peter <25291360@qq.com>
2016-08-25 18:23:45 +0800

    update all projects's of pom.xml for 2.0.8

--- common/src/main/java/com/me2me/common/utils/JPushUtils.java
+++ common/src/main/java/com/me2me/common/utils/JPushUtils.java
@@ -1,6 +1,9 @@
 package com.me2me.common.utils;
 
+import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Maps;
+import com.google.gson.JsonObject;
+import com.me2me.common.web.Specification;
 
 import java.util.Map;
 
@@ -12,8 +15,8 @@ import java.util.Map;
  */
 public class JPushUtils {
 
-    public static Map<String,String> packageExtra(Object object){
-        Map<String,String> map = Maps.newHashMap();
+    public static Map<String,Object> packageExtra(Object object){
+        Map<String,Object> map = Maps.newHashMap();
         map.put("extra",object.toString());
         return map;
     }
