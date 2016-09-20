a4278c7cea7e8c84ce30e9d713e81bf8aa72d948
peter <25291360@qq.com>
2016-09-20 18:37:43 +0800

    modify module for topic

--- user/src/main/java/com/me2me/user/service/UserServiceImpl.java
+++ user/src/main/java/com/me2me/user/service/UserServiceImpl.java
@@ -30,7 +30,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-import sun.security.tools.keytool.Resources_sv;
 
 import java.util.*;
 
