76fb58bbb998adc23957bff19f1a42db380b6e2c
peter <25291360@qq.com>
2016-09-20 18:38:39 +0800

    modify module for topic

--- user/src/main/java/com/me2me/user/service/UserServiceImpl.java
+++ user/src/main/java/com/me2me/user/service/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package com.me2me.user.service;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.gson.JsonObject;
