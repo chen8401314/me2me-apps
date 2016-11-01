6f8219d4c573affcd685ec56305fe54209abbcc6
maxiucheng <501942857@qq.com>
2016-11-01 17:51:44 +0800

    update ThirdPartLogin

--- common/src/main/java/com/me2me/common/web/ResponseStatus.java
+++ common/src/main/java/com/me2me/common/web/ResponseStatus.java
@@ -183,7 +183,9 @@ public enum  ResponseStatus {
 
     AWARD_MESSAGE_SUCCESS("中奖短信发送成功","award message success",20085),
 
-    GET_LIVE_UPDATE_SUCCESS("获取王国更新内容数量成功","get live update num success",20086),
+    THIRDPARTUSER_EXISTS("第三方账户已存在","thirdPartUser exists",20086),
+
+    GET_LIVE_UPDATE_SUCCESS("获取王国更新内容数量成功","get live update num success",20087),
 
 
 
