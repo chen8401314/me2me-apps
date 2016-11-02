9b4011b2c89dfadfcf582c5f5c0d4381c021685c
yiwu.wang <85812465@qq.com>
2016-11-02 11:23:14 +0800

    v2.1.4

--- content/src/main/java/com/me2me/content/dao/ContentMybatisDao.java
+++ content/src/main/java/com/me2me/content/dao/ContentMybatisDao.java
@@ -7,7 +7,7 @@ import com.me2me.content.dto.*;
 import com.me2me.content.mapper.*;
 import com.me2me.content.model.*;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
