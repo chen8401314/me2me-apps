0843c701967b6931f41dcc8f030299dd51907d6f
zhuchunlin <chunlin_2007@163.com>
2016-11-03 11:15:13 +0800

    mgmt add syslog query

--- operation-mgmt/src/main/java/com/me2me/mgmt/manager/MgmtSysLogManager.java
+++ operation-mgmt/src/main/java/com/me2me/mgmt/manager/MgmtSysLogManager.java
@@ -1,5 +1,9 @@
 package com.me2me.mgmt.manager;
 
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,5 +18,22 @@ public class MgmtSysLogManager {
 	
 	public void insert(MgmtSysLog log){
 		mgmtSysLogMapper.insert(log);
+	}
+	
+	public List<MgmtSysLog> queryPageByDescAndTime(String desc, Date startTime, Date endTime, int page, int pageSize){
+		if(StringUtils.isBlank(desc)){
+			desc = null;
+		}
+		if(page < 1){
+			page = 1;
+		}
+		if(pageSize < 1){
+			pageSize = 10;
+		}
+		int start = (page-1)*pageSize;
+		
+		List<MgmtSysLog> list = mgmtSysLogMapper.queryPageByDescAndTime(desc, startTime, endTime, start, pageSize);
+		
+		return list;
 	}
 }
