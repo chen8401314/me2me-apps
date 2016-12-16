36af12a92c7c601feccd846bc3c4c05e6bb2016a
Administrator <501942857@qq.com>
2016-12-16 14:07:47 +0800

    commit qitian

--- activity/src/main/java/com/me2me/activity/service/ActivityServiceImpl.java
+++ activity/src/main/java/com/me2me/activity/service/ActivityServiceImpl.java
@@ -1386,7 +1386,7 @@ public class ActivityServiceImpl implements ActivityService {
 
         activityMybatisDao.createAuser(auser);
         //发送短信 报名成功
-//        smsService.send7daySignUp(qiUserDto.getMobile());
+        //smsService.send7dayCommon("142378",qiUserDto.getMobile(),null,null);
         return Response.success(ResponseStatus.REGISTRATION_SUCCESS.status, ResponseStatus.REGISTRATION_SUCCESS.message);
     }
 
