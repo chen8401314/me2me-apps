f8f61e991b6889175b527a3113f3291f301ca08f
chenjipeng <666@qq.com>
2017-04-21 14:50:05 +0800

    add logger access by application

--- monitor-api/src/main/java/com/me2me/monitor/service/MonitorService.java
+++ monitor-api/src/main/java/com/me2me/monitor/service/MonitorService.java
@@ -1,6 +1,7 @@
 package com.me2me.monitor.service;
 
 import com.me2me.common.web.Response;
+import com.me2me.monitor.dto.AccessLoggerDto;
 import com.me2me.monitor.dto.MonitorReportDto;
 import com.me2me.monitor.event.MonitorEvent;
 import com.me2me.monitor.model.AccessTrack;
@@ -21,4 +22,6 @@ public interface MonitorService{
     Response loadActionReport(MonitorReportDto monitorReportDto);
 
     Response loadActivityReport(MonitorReportDto monitorReportDto);
+
+    void saveAccessLog(AccessLoggerDto accessLoggerDto);
 }
