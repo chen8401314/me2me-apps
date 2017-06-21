75550207f1f49ded82f169fd29644f0aba30d550
pc340 <pc340@pc340n.shmao.com>
2017-06-21 13:42:07 +0800

    ds

--- operation-mgmt/src/main/java/com/me2me/mgmt/controller/PriceController.java
+++ operation-mgmt/src/main/java/com/me2me/mgmt/controller/PriceController.java
@@ -424,7 +424,7 @@ public class PriceController {
 	
 	@RequestMapping(value = "/fullExecuteTask")
 	@ResponseBody
-	public String fullExecuteTask(){
+	public String fullExecuteTask() throws Exception{
 		kingdomPriceTask.executeFull();
 		return "执行完成";
 	}
