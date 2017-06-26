6ed6d2791e5619c3d5c4d4048f7cfbcc9aad4488
chenjipeng <666@qq.com>
2017-06-26 10:15:16 +0800

    modify bugs

--- content/src/main/java/com/me2me/content/widget/ContentLikes.java
+++ content/src/main/java/com/me2me/content/widget/ContentLikes.java
@@ -6,7 +6,14 @@ import com.me2me.common.web.Specification;
 import com.me2me.content.dto.LikeDto;
 import com.me2me.content.model.Content;
 import com.me2me.content.model.ContentLikesDetails;
+import com.me2me.user.dto.ModifyUserCoinDto;
+import com.me2me.user.rule.CoinRule;
+import com.me2me.user.rule.Rules;
+import com.me2me.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * 上海拙心网络科技有限公司出品
@@ -17,6 +24,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class ContentLikes extends AbstractLikes implements Likes {
 
+
+    @Autowired
+    private UserService userService;
 
 
     @Override
@@ -30,7 +40,12 @@ public class ContentLikes extends AbstractLikes implements Likes {
             if(contentLikes != null && likeDto.getAction() == Specification.IsLike.LIKE.index){
                 return Response.failure(ResponseStatus.CONTENT_USER_LIKES_ALREADY.status,ResponseStatus.CONTENT_USER_LIKES_ALREADY.message);
             }
-            return super.likes(likeDto);
+            CoinRule coinRule =userService.getCoinRules().get(Rules.LIKES_UGC_KEY);
+            coinRule.setExt(likeDto.getCid());
+            ModifyUserCoinDto modifyUserCoinDto = userService.coinRule(likeDto.getUid(),coinRule);
+            Response resp = super.likes(likeDto);
+            resp.setData(modifyUserCoinDto);
+            return resp;
         }else{
             return Response.failure(ResponseStatus.NO_RIGHTS_TO_LIKE.status,ResponseStatus.NO_RIGHTS_TO_LIKE.message);
         }
