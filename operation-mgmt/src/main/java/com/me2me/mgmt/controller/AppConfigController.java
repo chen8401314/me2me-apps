fd3c0284c6af85146793cdc1555272910b65654d
pc62 <pc62@pc342n.shmao.com>
2017-07-05 14:14:05 +0800

    bugfix

--- operation-mgmt/src/main/java/com/me2me/mgmt/controller/AppConfigController.java
+++ operation-mgmt/src/main/java/com/me2me/mgmt/controller/AppConfigController.java
@@ -10,7 +10,6 @@ import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.collections.map.LinkedMap;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
