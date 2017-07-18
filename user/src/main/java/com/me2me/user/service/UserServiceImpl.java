1146a15a5404b41a931411060b0c63dd891578e2
pc308 <pc308@pc308t.shmao.com>
2017-07-18 18:08:49 +0800

    bugfix

--- user/src/main/java/com/me2me/user/service/UserServiceImpl.java
+++ user/src/main/java/com/me2me/user/service/UserServiceImpl.java
@@ -4282,7 +4282,7 @@ public class UserServiceImpl implements UserService {
 			UserProfile userProfile = userMybatisDao.getUserProfileByUid(uid);
 			dto.setUid(uid);
 			dto.setNickName(userProfile.getNickName());
-			dto.setAvatar(userProfile.getAvatar());
+			dto.setAvatar(Constant.QINIU_DOMAIN + "/"+ userProfile.getAvatar());
 			return Response.success(dto);
 		}
 		
