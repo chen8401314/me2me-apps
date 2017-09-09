454829030d4af91fe6ddb1fa2363d65c2bee2484
pc340 <pc340@pc340n.shmao.com>
2017-09-09 18:50:29 +0800

    1

--- user/src/main/java/com/me2me/user/service/UserServiceImpl.java
+++ user/src/main/java/com/me2me/user/service/UserServiceImpl.java
@@ -4800,11 +4800,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public ModifyUserCoinDto modifyUserCoin(long uid , int coin) {
+    	log.info("111");
 	    ModifyUserCoinDto modifyUserCoinDto = new ModifyUserCoinDto();
 	    UserProfile userProfile = userMybatisDao.getUserProfileByUid(uid);
         modifyUserCoinDto.setCurrentLevel(userProfile.getLevel());
         int modifyCoin = userProfile.getAvailableCoin()+coin;
+        log.info("222");
 	    userInitJdbcDao.modifyUserCoin(uid,modifyCoin);
+	    log.info("333");
         String permissions = getAppConfigByKey(USER_PERMISSIONS);
         UserPermissionDto userPermissionDto = JSON.parseObject(permissions, UserPermissionDto.class);
         int lv = 0;
@@ -4817,6 +4820,7 @@ public class UserServiceImpl implements UserService {
             lv = 9;
         }
         if(lv <= userProfile.getLevel()){
+        	log.info("444");
             return modifyUserCoinDto;
         }else{
             for (UserPermissionDto.UserLevelDto userLevelDto : userPermissionDto.getLevels()) {
@@ -4827,6 +4831,7 @@ public class UserServiceImpl implements UserService {
                     break;
                 }
             }
+            log.info("555");
             return modifyUserCoinDto;
         }
     }
