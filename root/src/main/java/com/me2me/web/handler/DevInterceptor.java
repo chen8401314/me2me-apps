12722132782893767dc15c427d01af8185f66d26
pc340 <pc340@pc340n.shmao.com>
2017-09-19 18:10:04 +0800

    11

--- root/src/main/java/com/me2me/web/handler/DevInterceptor.java
+++ root/src/main/java/com/me2me/web/handler/DevInterceptor.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
-import org.elasticsearch.common.lang3.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
