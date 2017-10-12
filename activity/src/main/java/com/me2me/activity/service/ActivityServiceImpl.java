01362df98d1e9673dc37bc1cb9ebfd5e0dfece86
pc308 <pc308@pc308t.shmao.com>
2017-10-12 13:33:47 +0800

    手速活动方法加锁

--- activity/src/main/java/com/me2me/activity/service/ActivityServiceImpl.java
+++ activity/src/main/java/com/me2me/activity/service/ActivityServiceImpl.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.gson.internal.bind.TimeTypeAdapter;
 import com.me2me.activity.dao.ActivityMybatisDao;
 import com.me2me.activity.dao.LiveForActivityDao;
 import com.me2me.activity.dto.*;
@@ -5514,22 +5515,49 @@ public class ActivityServiceImpl implements ActivityService {
 	@Override
 	public Response gameResult(long uid, long gameId, int record) {
 		int coin = record>60?60:record;
-		//根据uid跟gameId去数据库中查询
-		GameUserRecord gameUserRecord = activityMybatisDao.getGameUserRecordByUidAndGameId(uid,gameId);
-		if(gameUserRecord != null){
-			//获取到数据库中原始的record
-			int originalRecord = gameUserRecord.getRecord();
-			if(record > originalRecord){
-				int originalCoin = originalRecord>60?60:originalRecord;
-				activityMybatisDao.updateGameUserRecordByIdAndRecord(gameUserRecord.getId(),record);
-				if(coin > originalCoin){
-					activityMybatisDao.updateGameUserInfoByGameIdAndCoins(gameId,coin-originalCoin);
+		String GAME_RESULT_KEY ="GAME_RESULT_KEY_"+uid+"_"+gameId;
+		try {
+			Boolean flag = true;
+			int stop = 0;
+			while(flag){
+				stop++;
+				if(cacheService.getLock(GAME_RESULT_KEY) > 0){
+					//根据uid跟gameId去数据库中查询
+					GameUserRecord gameUserRecord = activityMybatisDao.getGameUserRecordByUidAndGameId(uid,gameId);
+					if(gameUserRecord != null){
+						//获取到数据库中原始的record
+						int originalRecord = gameUserRecord.getRecord();
+						if(record > originalRecord){
+							int originalCoin = originalRecord>60?60:originalRecord;
+							activityMybatisDao.updateGameUserRecordAndCoinsByIdAndRecordAndCoins(gameUserRecord.getId(),record,coin);
+							if(coin > originalCoin){
+								activityMybatisDao.updateGameUserInfoByGameIdAndCoins(gameId,coin-originalCoin);
+							}
+						}
+					}else{
+						//如果数据库中不存在该记录，则创建一条新记录
+						activityMybatisDao.createNewGameRecordByUidAndGameIdAndRecordAndCoins(uid,gameId,record,coin);
+						activityMybatisDao.updateGameUserInfoByGameIdAndCoins(gameId,coin);
+					}
+					flag = false;
+				}else{
+					if(stop<100){
+						Timer timer = new Timer();
+						timer.schedule(new TimerTask() {
+							@Override
+							public void run() {
+								cacheService.getLock(GAME_RESULT_KEY);
+							}
+						},50);
+					}else{
+						return Response.failure(500,"无法获取锁");
+					}
 				}
-			}
-		}else{
-			//如果数据库中不存在该记录，则创建一条新记录
-			activityMybatisDao.createNewGameRecordByUidAndGameIdAndRecord(uid,gameId,record);
-			activityMybatisDao.updateGameUserInfoByGameIdAndCoins(gameId,record>60?60:record);
+			}	
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			cacheService.releaseLock(GAME_RESULT_KEY);
 		}
 		return Response.success(200, "OK");
 	}
