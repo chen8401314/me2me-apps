0b42076c3310c05ea66b7ce9061cf7923e9e14ce
pc339 <328692993@qq.com>
2017-12-26 11:49:35 +0800

    bugfix

--- content/src/main/java/com/me2me/content/service/ContentServiceImpl.java
+++ content/src/main/java/com/me2me/content/service/ContentServiceImpl.java
@@ -8779,15 +8779,16 @@ public class ContentServiceImpl implements ContentService {
 							tagContentElement.setType(53);
 							int tagCount = userService.getIntegerAppConfigByKey("HOME_HOT_LABELS");
 							if(CommonUtils.isNewVersion(version, "3.0.5")){//305版本以后的为该配置的3倍
-								tagCount = tagCount * 3;
-							}
-							if(CommonUtils.isNewVersion(version, "3.1.0")){//310版本以后的为该配置的2倍
-								tagCount = tagCount * 2;
+								if(CommonUtils.isNewVersion(version, "3.1.0")){//310版本以后的为该配置的2倍
+									tagCount = tagCount * 2;
+								}else{
+									tagCount = tagCount * 3;
+								}
 							}
 							tagContentElement.setSize(tagCount);
 							tagContentElement.setTitle("推荐标签在这里");
-							String tagTitle = userService.getAppConfigByKey("LIKE_TAG_TITLE");
-							String tagImage = Constant.QINIU_DOMAIN + "/" + userService.getAppConfigByKey("LIKE_TAG_IMAGE");
+							String tagTitle = userService.getAppConfigByKey("RECOMMEND_TAG_TITLE");
+							String tagImage = Constant.QINIU_DOMAIN + "/" + userService.getAppConfigByKey("RECOMMEND_TAG_IMAGE");
 							tagContentElement.setNickName(tagTitle);
 							tagContentElement.setAvatar(tagImage);
 							dto.getData().add(tagContentElement);
